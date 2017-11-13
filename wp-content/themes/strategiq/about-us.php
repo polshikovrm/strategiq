bb1dbbb6d7ed94c67bcc6d43bfcc685cbba05d39
Roman Polshikov <roman.polshikov@sigmalion.com.ua>
2017-11-13 12:57:56 +0200

    about-us page, main menu

--- wp-content/themes/strategiq/about-us.php
+++ wp-content/themes/strategiq/about-us.php
@@ -15,9 +15,7 @@
     </div>
     <div class="row">
       <div class="col-sm-10 col-sm-push-1 text-center">
-        <p>StrategiQ Capital (Pty) Ltd ("StrategiQ") was founded in 2015 with the culmination of like-minded investment professionals seeking to actively manage investors wealth and in so doing fulfilling a desire to build a world class investment company. </p>
-        <p>We believe that our mandate is not limited to investment performance alone, but also in playing an active role in assisting our clients in developing an overarching wealth management strategy. This will only be achieved by building meaningful relationships with our clients, thus facilitating a seamless transfer of assets from generation to generation, ensuring an uninterrupted continuity of their family's legacy and business interests.</p>
-        <p>It is our strong belief that as guardians of the capital entrusted to us we invest our money alongside our investors ensuring an alignment of interests.</p>
+       <?php the_content() ?>
       </div>
     </div>
   </div>
@@ -28,8 +26,7 @@
           <div class="title-block">
               <h1>Our Services</h1>
           </div>
-          <p>StrategiQ is an approved discretionary investment manager (i.e. a Category II Financial Services Provider, FSP Licence No 46624) that is independently managed, focussed on providing investors with consistent real returns whilst preserving capital over the long term. Our service and reporting to our clients takes two forms, written and personal.</p>
-          <p>We produce quarterly reports which cover the respective portfolio's performance and risk statistics. We like to meet frequently with clients to stay close to their portfolio objectives and any changes that may need to be considered in the management of their portfolios. Our Weekly Review emailed each week keeps our clients abreast of current market conditions.</p>
+          <?php the_field('our_services')?>
       </div>
   </div>
   <div class="image-holder">
@@ -45,17 +42,7 @@
           <div class="title-block">
               <h1>Investment Style</h1>
           </div>
-          <p>At StrategiQ Capital we understand that every investor is different, requiring a personalised investment strategy that will meet their long-term goals and objectives.</p>
-          <p>Our investment philosophy and approach is governed by the following key principles:</p>
-          <ul>
-              <li>We are a discretionary asset manager (we assume responsibilitty in managing our client's portfolios);</li>
-              <li>We focus on identifying high-quality investments because it makes sense to us and fits our risk averse-personalities;</li>
-              <li>We are active managers, focused on providing clients with consistent real returns whilst seeking to preserve their capital over the long-term;</li>
-              <li>We follow an unconstrained, high conviction approach to investing which proves to generate additional returns in the long-term;</li>
-              <li>We leverage specialist investment professionals utilising their skill and knowledge where it can add value;</li>
-              <li>We ensure effective diversification at all levels (across asset classes and geographies); and</li>
-              <li>We structure portfolios to be cost and tax effective.</li>
-          </ul>
+          <?php the_field('investment_style')?>
       </div>
   </div>
 </section>
@@ -65,13 +52,7 @@
           <div class="title-block">
               <h1>Why Us</h1>
           </div>
-          <ul>
-              <li>Owner-managed and independent, allowing us the necessary flexibility to put clients first;</li>
-              <li>A dynamic investment process that is personalised to the unique needs of each client;</li>
-              <li>Highly qualified and experienced investment team delivering institutional skills to private clients;</li>
-              <li>Market and industry insights; and</li>
-              <li>The highest standards of client service.</li>
-          </ul>
+          <?php the_field('why_us') ?>
       </div>
   </div>
     <div class="image-holder">
