95a3de3b6f0b222dfc6d4386cd424e692f79f942
Sorokina Anna <anna.sorokina@sigmalion.com.ua>
2017-11-13 12:59:23 +0200

    #7892 Family Office page

---
+++ wp-content/themes/strategiq/family-office.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Template Name: Family Office
+ */
+?>
+<section class="visual-section" style="background-image: url(<?php echo get_template_directory_uri(); ?>/dist/images/bg-visual-office.jpg)">
+  <div class="container">
+    <h1>Family Office</h1>
+  </div>
+</section>
+<section class="section content-section">
+  <div class="container">
+    <div class="title-block text-center">
+      <h1>Introduction</h1>
+    </div>
+    <div class="row">
+      <div class="col-sm-10 col-sm-push-1 text-center">
+          <p>StrategiQ Family Office is a premier multi-family office, managing the affairs of multiple wealthy families. Our approach entails holistic and personalised portfolio management services, the creation of a family mission statement, investment architecture tailored to each family's needs, ongoing review of investment strategies, portfolio aggregation and reporting services and oversight of third party managers. We integrate disciplined risk assessment and portfolio risk management throughout the process.</p>
+          <p>We serve many types of high net worth individuals, ranging from business owners, entrepreneurs and other creators of first-generation wealth. </p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section purple-bg twocolumn-section">
+    <div class="text-holder">
+        <div class="text">
+            <div class="title-block">
+                <h1>Family Office Services</h1>
+            </div>
+            <p>We help you develop an integrated long-term wealth plan that considers multiple aspects of your life.</p>
+            <ul>
+                <li>Developing an Investment Plan</li>
+                <li>Defining your legacy</li>
+                <li>Preparing the Next Generation</li>
+                <li>Tax & Estate-Planning Opportunities</li>
+                <li>Property Advisory</li>
+                <li>Structuring and Protecting Assets </li>
+                <li>Concentrated Holding Strategies</li>
+                <li>Accessing Hedge Funds and Private Equity</li>
+                <li>Corporate Finance</li>
+                <li>Philanthropy and impact investing</li>
+            </ul>
+            <p>We work with you and your family to form an appropriate strategy based on three principles of governance.</p>
+            <div class="services-block">
+                <ul>
+                    <li><a href="#">Investment Governance</a></li>
+                    <li><a href="#">Operational Governance</a></li>
+                    <li><a href="#">Family Governance</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="image-block gray-bg">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/img-04.png" alt="image description">
+    </div>
+</section>
+<section class="section light-purple-bg twocolumn-section">
+    <div class="image-holder width-2">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/img-05.jpg" alt="image description">
+    </div>
+    <div class="text-holder width-3">
+        <div class="text">
+            <div class="title-block">
+                <h1>Our Purpose</h1>
+            </div>
+            <p>A family office acts as a private management team to wealthy families, with a dedicated team of professional advisers who oversee clients' complete financial affairs.</p>
+            <p>An investment committee is established comprising of 3-5 members (family members, outside advisors & investment professionals). The committee meets on a semi-annual basis with actionable agendas circulated in advance, formalising the management process. </p>
+            <p>A family mission statement or policy statement provides a framework for the family to "buy into" the investment strategy. The Investment Committee has numerous responsibilities including asset allocation, fund manager selection and the review of overall investment performance.</p>
+        </div>
+    </div>
+</section>
+<section class="section gray-bg twocolumn-section">
+  <div class="text-holder">
+      <div class="text">
+          <div class="text-block">
+              <div class="title-block">
+                  <h1>Our Team and Network</h1>
+              </div>
+              <p>We combine our clients with a network of highly skilled and complementary partners, including proven global investment managers, tax specialists, accountants and legal experts.</p>
+          </div>
+          <div class="text-block">
+              <div class="title-block">
+                  <h1>Investment Committee</h1>
+                  <p>A team of industry experts in their respective fields (Investments, Legal, Accounting, Regulatory & other) meet on a quarterly basis to discuss opportunities and trends that may need to be addressed by the respective family houses.</p>
+              </div>
+          </div>
+          <div class="text-block">
+              <div class="title-block">
+                  <h1>Benefits</h1>
+              </div>
+              <ul>
+                  <li>Holistic portfolio view with advice provided with a complete understanding of all the family assets and liabilities</li>
+                  <li>Benefits of combined purchasing power, allowing for reduced fees and costs</li>
+                  <li>More control and confidence in decision-making</li>
+                  <li>Access to institutional investment opportunities </li>
+                  <li>Services are provided to the entire high net-worth family</li>
+                  <li>Truly "independent advice"</li>
+              </ul>
+          </div>
+      </div>
+  </div>
+    <div class="image-holder">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/img-06.jpg" alt="image description">
+    </div>
+</section>
